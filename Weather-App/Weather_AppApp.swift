2bc0093133e44f84bfeb58e183fc5188c6dac1b1
Fadysameh2020 <fadysameh10@gmail.com>
2024-07-14 16:31:17 +0300

    first commit

---
+++ Weather-App/Weather_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Weather_AppApp.swift
+//  Weather-App
+//
+//  Created by Fady Sameh on 7/14/24.
+//
+
+import SwiftUI
+
+@main
+struct Weather_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
